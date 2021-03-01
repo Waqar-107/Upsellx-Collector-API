4279888455a2223b7b0507d9e1358cd29a03a9c0
Waqar-107 <waqar.hassan866@gmail.com>
2021-03-01 22:35:30 +0600

    updated route, controller now has the company website url from param

--- controllers/company.js
+++ controllers/company.js
@@ -4,4 +4,8 @@ mongoose.set("useFindAndModify", false);
 const {SUCCESS, INTERNAL_SERVER_ERROR, BAD_REQUEST, DATA_NOT_FOUND} = require("../util/errors");
 const error_message = require("../util/error_message");
 
-exports.scrape_company_data = async function (req, res) {};
+exports.scrape_company_data = async function (req, res) {
+	const company_url = req.query.url;
+
+	res.status(SUCCESS).json({msg: "success"});
+};
--- routes/company.js
+++ routes/company.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 const companyController = require("../controllers/company");
 
+router.get("/api/get_data", companyController.scrape_company_data);
 module.exports = router;
