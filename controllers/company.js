74595434625576aa85923212889782b655fb1352
Waqar-107 <waqar.hassan866@gmail.com>
2021-03-02 00:05:24 +0600

    updated controller

--- controllers/company.js
+++ controllers/company.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
+const CompanyModel = require("../models/company");
 
 const {SUCCESS, INTERNAL_SERVER_ERROR, BAD_REQUEST, DATA_NOT_FOUND} = require("../util/errors");
 const error_message = require("../util/error_message");
@@ -7,5 +8,13 @@ const error_message = require("../util/error_message");
 exports.scrape_company_data = async function (req, res) {
 	const company_url = req.query.url;
 
-	res.status(SUCCESS).json({msg: "success"});
+	const company_data = await CompanyModel.findOne({website_url: company_url});
+	if (company_data) {
+		res.status(SUCCESS).json({msg: "success", company_data});
+	}
+
+	// fetch data, analyze and save it
+	else {
+		res.status(DATA_NOT_FOUND).json({msg: "looking"});
+	}
 };
