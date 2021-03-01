e191ab33a2022a5bee8bdd6be4620e31c072882c
Waqar-107 <waqar.hassan866@gmail.com>
2021-03-02 01:35:11 +0600

    jsdom worked fine - tried out chaldal.com

--- controllers/company.js
+++ controllers/company.js
@@ -5,6 +5,10 @@ const CompanyModel = require("../models/company");
 const {SUCCESS, INTERNAL_SERVER_ERROR, BAD_REQUEST, DATA_NOT_FOUND} = require("../util/errors");
 const error_message = require("../util/error_message");
 
+const got = require("got");
+const jsdom = require("jsdom");
+const {JSDOM} = jsdom;
+
 exports.scrape_company_data = async function (req, res) {
 	const company_url = req.query.url;
 
@@ -15,6 +19,15 @@ exports.scrape_company_data = async function (req, res) {
 
 	// fetch data, analyze and save it
 	else {
+		got(company_url)
+			.then((response) => {
+				const dom = new JSDOM(response.body);
+				console.log(dom.window.document.querySelector("title").textContent);
+			})
+			.catch((err) => {
+				res.status(INTERNAL_SERVER_ERROR).json({message: "error fetching site :" + err});
+			});
+
 		res.status(DATA_NOT_FOUND).json({msg: "looking"});
 	}
 };
